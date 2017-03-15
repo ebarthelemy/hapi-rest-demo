9395c25821c9d4b71c619e05c334bf6aa39c5474
Edouard Barthelemy <edouard@liveanswer.com>
2017-03-15 16:57:04 -0400

    CI Test 2

--- startApp.sh
+++ startApp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=spidertutorial-postgresql.chghigskkvkd.us-west-2.rds.amazonaws.com:5432
+export DB_PRD_HOST=spidertutorial-postgresql.chghigskkvkd.us-west-2.rds.amazonaws.com
 export DB_PRD_USER=spidertutorial
 export DB_PRD_PASS=spidertutorial
 export NODE_HOST=localhost
